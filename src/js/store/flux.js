55b5a96724db7dedd1e28850e1111c297b62936a
Solracort <solracort@gmail.com>
2023-02-02 20:23:13 +0000

    primer paso trasladar a store funciones y estados

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,73 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			personajes: [],
+			planetas:[],
+			vehiculos:[],
+			params : [],
+    		caracteristica:[],
+    		description:[],
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+				getAPIName: ()=>{
+					fetch("https://www.swapi.tech/api/people/")
+					.then(res => res.json())
+					//.then(data => console.log(data))
+					.then(data =>{ 
+						//console.log(data);
+						setPersonaje(data.results)
+					})
+					.catch(err => console.error(err))
+				},
+				getPlanet:()=>{
+					fetch("https://www.swapi.tech/api/planets/")
+					.then(res => res.json())
+					.then(data => {
+						console.log(data)
+						setPlanet(data.results)})
+					.catch(err => console.error(err))
+				},
+				getVehicle:()=>{
+					fetch("https://www.swapi.tech/api/vehicles/")
+					.then(res => res.json())
+					.then(data => {
+						console.log(data)
+						setVehicle(data.results)})
+					.catch(err => console.error(err))
+				},
+				getAPIcaracteristicas:()=>{
+					fetch("https://www.swapi.tech/api/vehicles/"+props.id)
+					.then(res => res.json())
+					.then(data =>{ 
+						//console.log("esto es vehicles"+ data);
+						setCaracteristica(data.result.properties)
+					})
+					.catch(err => console.error(err))
+					},
+				getAPIcaracteristicas:()=>{
+						fetch("https://www.swapi.tech/api/people/"+props.id)
+						.then(res => res.json())
+						.then(data =>{ 
+							//console.log("esto es"+ data);
+							setCaracteristica(data.result.properties)
+						})
+						.catch(err => console.error(err))
+				},
+				getAPIcaracteristicas:()=>{
+					fetch("https://www.swapi.tech/api/planets/"+props.id)
+					.then(res => res.json())
+					.then(data =>{ 
+						//console.log("Planeta"+ data.result);
+						setCaracteristica(data.result)
+					})
+					.catch(err => console.error(err))
+					},
 			}
 		}
-	};
-};
+	}
+
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useContext} from "react";
+import {Context} from "../store/appContext"
 import "../../styles/home.css";
 import {Cards} from "../component/cards.jsx"
 import {CardPlanet} from "../component/cardPlanet.jsx"
@@ -6,41 +7,10 @@ import {CardVehicle} from "../component/cardVehicle.jsx"
 
 export const Home = () => {
 
-	const [personaje,setPersonaje] = useState([])
-	const [vehicle,setVehicle] = useState([])
-	const [planet,setPlanet] = useState([])
+	console.log(useContext(Context))
 
-    function getAPIName(){
-        fetch("https://www.swapi.tech/api/people/")
-        .then(res => res.json())
-        //.then(data => console.log(data))
-		.then(data =>{ 
-			//console.log(data);
-			setPersonaje(data.results)
-		})
-        .catch(err => console.error(err))
-    }
-	function getPlanet(){
-		fetch("https://www.swapi.tech/api/planets/")
-		.then(res => res.json())
-		.then(data => {
-			console.log(data)
-			setPlanet(data.results)})
-		.catch(err => console.error(err))
-	}
-	function getVehicle(){
-		fetch("https://www.swapi.tech/api/vehicles/")
-		.then(res => res.json())
-		.then(data => {
-			console.log(data)
-			setVehicle(data.results)})
-		.catch(err => console.error(err))
-	}
-    useEffect(()=>{
-		getAPIName();
-		getVehicle();
-		getPlanet();        
-    },[])
+    
+    
 	
 
 return(
@@ -48,19 +18,19 @@ return(
 		<h2 className="text-center">Personajes</h2>
 		<div className="container text-center">
 			<div className="row row-cols-xs-1 row-cols-sm-3 row-cols-md-6 m-auto">
-				{personaje.map((item)=><Cards key={item.uid} id={item.uid} nombre={item.name} />)}
+				{/* {personaje.map((item)=><Cards key={item.uid} id={item.uid} nombre={item.name} />)} */}
 			</div>
 		</div>
 		<h2 className="text-center">Vehicles</h2>
 		<div className="container text-center">
 			<div className="row row-cols-xs-1 row-cols-sm-3 row-cols-md-6 m-auto">
-				{vehicle.map((item)=><CardVehicle key={item.uid} id={item.uid} nombre={item.name} />)}
+				{/* {vehicle.map((item)=><CardVehicle key={item.uid} id={item.uid} nombre={item.name} />)} */}
 			</div>
 		</div>
 		<h2 className="text-center">Planets</h2>
 		<div className="container text-center">
 			<div className="row row-cols-xs-1 row-cols-sm-3 row-cols-md-6 m-auto">
-				{planet.map((item)=><CardPlanet key={item.uid} id={item.uid} nombre={item.name} />)}
+				{/* {planet.map((item)=><CardPlanet key={item.uid} id={item.uid} nombre={item.name} />)} */}
 			</div>
 		</div>
 	</>
